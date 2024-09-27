d67e46985465029bfb67c68c904ab5d137f368b2
Alex Monteiro <alex.monteiro@digiage.com.br>
2024-09-26 23:04:10 -0300

    add controllers

--- src/modules/products/repositories/IProductsRepository.ts
+++ src/modules/products/repositories/IProductsRepository.ts
@@ -3,7 +3,8 @@ import { Product } from "../infra/typeorm/entities/Product";
 
 interface IProductsRepository {
     create(data: IProductsDTO): Promise<void>;
-    findById(id: string): Product<Product>;
+    findById(id: string): Promise<Product>;
+    find(): Promise<Product[]>;
 }
 
 export { IProductsRepository };
--- src/shared/infra/http/routes/products.route.ts
+++ src/shared/infra/http/routes/products.route.ts
@@ -1,6 +1,12 @@
 import { Router } from "express";
 
+import CreateProductController from "@modules/products/useCases/createProduct/CreateProductController";
+
 const productRoutes = Router();
+
+const createProductController = new CreateProductController();
+
+productRoutes.post("/", createProductController.handle);
 
 
 export { productRoutes };
